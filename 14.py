599ec6d43fbc721d0846087b72c1e1ea0ec266b1
Nandhin98 <47237438+Nandhin98@users.noreply.github.com>
2019-03-27 16:23:45 +0530

    Create 14.py

---
+++ 14.py
@@ -0,0 +1,8 @@
+from string import ascii_lowercase as asc_lower
+           def checks(s):
+                           return set (asc_lower)-set(s.lower())==set([])
+              str1=input("enter string:")
+              if(check(str)==true):
+                  print("the string is a program")
+              else:
+                       print("the string isn't a pangram")
